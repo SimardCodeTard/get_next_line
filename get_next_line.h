aea00dd83666cc7de34ea4462a4e54b13be804e9
smenard <floogles71@gmail.com>
2025-11-19 17:13:43 +0100

    feat(*): header

--- get_next_line.h
+++ get_next_line.h
@@ -5,13 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/13 15:08:46 by smenard           #+#    #+#             */
-/*   Updated: 2025/11/19 11:18:32 by smenard          ###   ########.fr       */
+/*   Created: 2025/11/19 11:29:37 by smenard           #+#    #+#             */
+/*   Updated: 2025/11/19 15:20:25 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-
+#ifndef	GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
@@ -19,7 +18,7 @@
 # endif
 
 # ifndef MAX_FD
-#  define MAX_FD 64
+#  define MAX_FD 1024
 # endif
 
 # include <unistd.h>
@@ -29,34 +28,22 @@
 # include <stdbool.h>
 # include <stdint.h>
 
-enum e_extract_next_line_result
-{
-	BUFFER_FINISHED,
-	ERROR,
-	LINE_COMPLETE
-};
-
-enum e_read_result
-{
-	READ_END = 0,
-	READ_ERROR = -1,
-};
-
-typedef struct s_buffer
-{
-	char		*data;
-	uint32_t	index;
-}	t_buffer;
+/* ===== get_next_line.c ===== */
 
 char	*get_next_line(int fd);
 
-void	*ft_memmove(void *dest, void *src, size_t n);
+char	*get_rest(int fd);
+
+/* ===== get_next_line_utils.c ===== */
 
 size_t	next_line_len(char *buffer);
 
-void	extract_next_line(int fd, char **line_buffer, t_buffer *buffer,
-			size_t len);
+char	*extract_line(int fd, char *buffer);
 
-ssize_t	read_exact(int fd, t_buffer *buffer, uint64_t size);
+size_t	read_exact(int fd, char *buffer, size_t size);
+
+char	*ft_strjoin(char *s1, char *s2);
+
+void	*free_return(void **ptrs, void *value);
 
 #endif
